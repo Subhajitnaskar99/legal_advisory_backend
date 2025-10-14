e53db490b7e4547adb3f5e3921f2c7f6633e4b64
SUBHAJIT NASKAR <subhajitnaskar99@gmail.com>
2025-10-14 14:09:31 +0530

    Get method API added

--- app/routes/users.py
+++ app/routes/users.py
@@ -7,11 +7,32 @@ from app.config import settings
 from bson.objectid import ObjectId
 router = APIRouter(prefix="/users", tags=["users"])
 
-@router.get("/me", response_model=UserPublic)
-async def get_me(user = Depends(get_current_user)):
-    return {"email": user["email"], "full_name": user.get("full_name"), "role": user.get("role")}
-
-@router.put("/update_basic_info")
+@router.get("/me")
+async def get_me(authorization: str = Header(...)):
+    token = authorization.replace("Bearer ", "")
+    payload = decode_token(token)
+    #print(payload)
+    if not payload:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+    user_id = payload.get("sub")
+    if not user_id:
+        raise HTTPException(status_code=400, detail="Invalid token payload")
+    user = await users_collection.find_one({"_id": ObjectId(user_id)})
+    if not user:    
+        raise HTTPException(status_code=404, detail="User not found")
+    return {
+        "status": "success",
+        "message": "User details updated successfully",
+        "token": token,
+        "email": user.get("email"),
+        "full_name": user.get("full_name"),
+        "role": user.get("role"),
+        "phone": user.get("phone"),
+        "address": user.get("address"),
+        "dob": user.get("dob"),
+        "gender": user.get("gender")
+    }
+@router.put("/update_basic_info") 
 async def update_basic_info(
     user: UserDetails,
     authorization: str = Header(...)
